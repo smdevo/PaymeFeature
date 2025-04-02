5c67bae7b13c4c5512e72571d205ec649e1c1c66
Samandar <samandartoshpulatov0704@gmail.com>
2025-04-02 14:01:02 +0500

    Initial Commit

---
+++ PaymeFeature/PresentationLayer/UniteScreen(Tabbar)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymeFeature
+//
+//  Created by Samandar on 02/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
