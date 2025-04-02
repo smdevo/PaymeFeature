464a202bcf49c569c210119793bc91703704fccb
Samandar <samandartoshpulatov0704@gmail.com>
2025-04-02 15:45:03 +0500

    Adding Views

---
+++ PaymeFeature/PresentationLayer/MainScreen/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  PaymeFeature
+//
+//  Created by Samandar on 02/04/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
---
+++ PaymeFeature/PresentationLayer/PaymentsScreen/PaymentsView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentsView.swift
+//  PaymeFeature
+//
+//  Created by Samandar on 02/04/25.
+//
+
+import SwiftUI
+
+struct PaymentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentsView()
+}
