542ba7a94f23f98eef2c8e58bc433a56f6f3def5
UmidKhan <ufayzimatov4@gmail.com>
2025-04-08 18:09:40 +0500

    added function for formatting card number

--- PaymeFeature/PresentationLayer/TransfersScreen/TransfersView.swift
+++ PaymeFeature/PresentationLayer/TransfersScreen/TransfersView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
  
-
 struct TransfersView: View {
     @State private var cardOrPhone: String = ""
 
+    func formatCardNumber(_ number: String) -> String {
+        let cleanNumber = number.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        
+        let truncatedNumber = String(cleanNumber.prefix(16))
+        
+        var formattedNumber = ""
+        
+        for (index, char) in truncatedNumber.enumerated() {
+            if index != 0 && index % 4 == 0 {
+                formattedNumber.append(" ")
+            }
+            formattedNumber.append(char)
+        }
+        
+        return formattedNumber
+    }
+
+    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading, spacing: 20) {
                 HStack {
+                    Spacer()
                     Text("Pul o’tkazmasi")
-                        .font(.title2)
+                        .font(.title3)
                         .bold()
                     Spacer()
                     NavigationLink(destination: InfoScreen()) {
@@ -26,15 +44,26 @@ struct TransfersView: View {
                     }
                 }
                 .padding(.top)
-
+                
                 Text("Kimga:")
                     .font(.headline)
-
+                    .padding(.top, 30)
+                Text("Karta yoki telefon raqami")
+                    .foregroundStyle(.gray)
+                    .font(.system(size:14))
+                    .padding(.bottom, -20)
                 HStack {
                     Image(systemName: "creditcard")
                         .foregroundColor(.gray)
                     TextField("Karta yoki telefon raqami", text: $cardOrPhone)
                         .keyboardType(.numberPad)
+                        .onChange(of: cardOrPhone) { newValue in
+                            cardOrPhone = formatCardNumber(newValue)
+                        }
+                        .font(.system(size: 14))
+//                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
                     Button(action: {
                     }) {
                         Image(systemName: "qrcode.viewfinder")
@@ -62,10 +91,13 @@ struct TransfersView: View {
                 VStack(spacing: 12) {
                     NavigationLink(destination: PhoneTransferScreen()) {
                         HStack {
-                            Image(systemName: "phone")
+                            Image(systemName: "phone.fill")
+                                .foregroundColor(.paymeC)
                             Text("Telefon raqami bo`yicha")
+                                .foregroundColor(.black)
                             Spacer()
                             Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
                         }
                         .padding()
                         .background(Color(.systemGray6))
@@ -75,9 +107,12 @@ struct TransfersView: View {
                     NavigationLink(destination: SelfTransferScreen()) {
                         HStack {
                             Image(systemName: "creditcard")
+                                .foregroundColor(.paymeC)
                             Text("Mening kartamga o’tkazish")
+                                .foregroundColor(.black)
                             Spacer()
                             Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
                         }
                         .padding()
                         .background(Color(.systemGray6))
@@ -87,19 +122,23 @@ struct TransfersView: View {
                     NavigationLink(destination: GreetingScreen()) {
                         HStack {
                             Image(systemName: "gift")
+                                .foregroundColor(.paymeC)
                             Text("Tabriknoma qo'shish")
+                                .foregroundColor(.black)
                             Spacer()
                             Image(systemName: "plus")
+                                .foregroundStyle(.gray)
                         }
                         .padding()
                         .background(Color(.systemGray6))
                         .cornerRadius(12)
                     }
                 }
+                
 
                 Spacer()
             }
-            .padding()
+            .padding([.bottom, .leading, .trailing],15)
         }
     }
 }
