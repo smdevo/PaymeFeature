545a299e47910fcb070b0dc49baba8db4595c60c
sxmpiternvl <sxmpiternvl@icloud.com>
2025-04-16 14:43:47 +0500

    familyView UI , bottomsheets

--- PaymeFeature/PresentationLayer/FamilyScreen/Views/BottomSheets/AddFamilyMember.swift
+++ PaymeFeature/PresentationLayer/FamilyScreen/Views/BottomSheets/AddFamilyMember.swift
@@ -5,84 +5,103 @@
 //  Created by Dmitriy An on 08/04/25.
 //
 
-
 import SwiftUI
 
 struct AddFamilyMember: View {
     @ObservedObject var viewModel: FamilyViewModel
+    @Binding var showSnackbar: Bool
     
     @State private var phoneNumber: String = ""
-    @State private var showSMSAlert: Bool = false
     @State private var isButtonLoading: Bool = false
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(spacing: 0) {
-            ScrollView {
-                VStack(spacing: 16) {
-                    LabeledTextField(
-                        label: "Номер телефона",
-                        placeholder: "Введите номер телефона",
-                        text: $phoneNumber,
-                        keyboardType: .phonePad
-                    )
-                    .onChange(of: phoneNumber) { newValue, _ in
-                        var result = ""
-                        if let firstChar = newValue.first, firstChar == "+" {
-                            result = "+"
-                        }
-                        let digits = newValue.filter { $0.isNumber }
-                        let limitedDigits = String(digits.prefix(12))
-                        let formatted = result + limitedDigits
-                        if formatted != newValue {
-                            phoneNumber = formatted
-                        }
-                    }
-                    
-                    if isButtonLoading {
-                        ProgressView()
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                    } else {
-                        Button(action: {
-                            guard !phoneNumber.isEmpty else { return }
-                            guard let currentUser = viewModel.currentUser else { return }
-                            
-                            isButtonLoading = true
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                                viewModel.sendInvitation(phoneNumber: phoneNumber, adminUser: currentUser) { success in
-                                    isButtonLoading = false
-                                    if success {
-                                        showSMSAlert = true
-                                    }
-                                }
+        ZStack {
+            VStack(spacing: 0) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        Text("Пригласить в семью")
+                            .font(.title)
+                        
+                        LabeledTextField(
+                            label: "Номер телефона",
+                            placeholder: "Введите номер телефона",
+                            text: $phoneNumber,
+                            keyboardType: .phonePad
+                        )
+                        .onChange(of: phoneNumber) { newValue, _ in
+                            var result = ""
+                            if let firstChar = newValue.first, firstChar == "+" {
+                                result = "+"
                             }
-                        }) {
-                            Text("Пригласить в семью")
-                                .font(.headline)
-                                .foregroundColor(.white)
+                            let digits = newValue.filter { $0.isNumber }
+                            let limitedDigits = String(digits.prefix(12))
+                            let formatted = result + limitedDigits
+                            if formatted != newValue {
+                                phoneNumber = formatted
+                            }
+                        }
+                        
+                        if isButtonLoading {
+                            ProgressView()
                                 .padding()
                                 .frame(maxWidth: .infinity)
-                                .background(Color.blue)
-                                .cornerRadius(10)
+                        } else {
+                            Button(action: {
+                                guard !phoneNumber.isEmpty else { return }
+                                guard let currentUser = viewModel.currentUser else { return }
+                                
+                                isButtonLoading = true
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                                    viewModel.sendInvitation(phoneNumber: phoneNumber, adminUser: currentUser) { success in
+                                        isButtonLoading = false
+                                        if success {
+                                            dismiss()
+
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                                                showSnackbar = true
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }) {
+                                Text("Пригласить в семью")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(.paymeC)
+                                    .cornerRadius(10)
+                            }
+                            .padding(.horizontal)
+                            .padding(.top, 10)
                         }
-                        .padding(.horizontal)
-                        .padding(.top, 10)
-                        
                     }
+                    .padding()
                 }
-                .padding()
+                Spacer(minLength: 0)
             }
-            Spacer(minLength: 0)
+            
+//            if showSnackbar {
+//                VStack {
+//                    Spacer()
+//                    HStack {
+//                        Image(systemName: "checkmark.circle.fill")
+//                            .foregroundColor(.white)
+//                        Text("Приглашение отправлено ребёнку.")
+//                            .foregroundColor(.white)
+//                    }
+//                    .padding()
+//                    .background(Color.green)
+//                    .cornerRadius(12)
+//                    .padding(.bottom, 20)
+//                    .transition(.move(edge: .bottom).combined(with: .opacity))
+//                    .animation(.easeInOut(duration: 0.3), value: showSnackbar)
+//                }
+//            }
         }
-        .background(Color(UIColor.systemBackground))
-        .alert("Подтверждение SMS", isPresented: $showSMSAlert) {
-            Button("OK") {
-                dismiss()
-            }
-        } message: {
-            Text("На указанный номер отправлено уведомление с кодом")
-        }
+        .background(.backgroundC)
     }
 }
---
+++ PaymeFeature/PresentationLayer/MainScreen/CardsScreen/SubViews/ChildCardView.swift
@@ -0,0 +1,108 @@
+//
+//  ChildCardView.swift
+//  PaymeFeature
+//
+//  Created by Dmitriy An on 16/04/25.
+//
+
+import SwiftUI
+
+
+struct ChildCardView: View {
+    
+    @State private var showParentServiceSheet = false
+    @State private var showChildServiceSheet = false
+    
+    @EnvironmentObject var vm: GlobalViewModel
+    let bankCard: BankCard
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            Text(bankCard.name)
+                .font(.title)
+                .foregroundColor(.white)
+                .textWithBlackBorder()
+            
+            Spacer()
+            
+            Text("\(bankCard.sum) сум")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .textWithBlackBorder()
+            
+            HStack {
+                Image("paymekids")
+                       .resizable()
+                       .scaledToFit()
+                       .frame(height: 110)
+                
+                Spacer()
+                
+                Image("kids")
+                       .resizable()
+                       .scaledToFit()
+                       .frame(height: 80)
+            }
+        }
+        .padding()
+        .background {
+            Group {
+                if  !bankCard.isFamilyCard, vm.currentUser?.role == true {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.indigo.opacity(0.6), .paymeC]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                } else {
+                    Image("girlBackground")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .shadow(color: .black.opacity(0.1), radius: 8, x: 0, y: 4)
+            .clipped()
+        }
+        .foregroundStyle(.white)
+        .cornerRadius(16)
+        
+        .sheet(isPresented: $showParentServiceSheet) {
+            ServicesSheetViewForParent()
+                .presentationDetents([.fraction(0.6)])
+                .presentationDragIndicator(.visible)
+        }
+        .sheet(isPresented: $showChildServiceSheet) {
+            ServicesSheetViewForChild()
+                .presentationDetents([.fraction(0.6)])
+                .presentationDragIndicator(.visible)
+        }
+        .onTapGesture {
+            if bankCard.isFamilyCard {
+                guard let cUser = vm.currentUser else { return }
+                if cUser.role {
+                    showParentServiceSheet.toggle()
+                } else {
+                    showChildServiceSheet.toggle()
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func textWithBlackBorder() -> some View {
+        self.shadow(color: .black, radius: 1, x: 1, y: 1)
+    }
+}
+
+//#Preview {
+//    let model: BankCard = .init(name: "Apple Inc.", ownerName: "Apple Inc.", sum: "100", cardNumber: "100", type: .humo, expirationDate: "sfds")
+//    ChildCardView(bankCard: model)
+//        .environmentObject(GlobalViewModel())
+//        .padding()
+//}
