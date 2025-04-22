4713c8d474470482cae890850affc6c1616e6254
Samandar <samandartoshpulatov0704@gmail.com>
2025-04-22 17:40:05 +0500

    Fixing family View model environment object problem

--- PaymeFeature/PresentationLayer/MainScreen/FamilyScreen/Views/TransferSheetsSheets/ServiceSheetViews/ServicesSheetView.swift
+++ PaymeFeature/PresentationLayer/MainScreen/FamilyScreen/Views/TransferSheetsSheets/ServiceSheetViews/ServicesSheetView.swift
@@ -143,41 +143,41 @@ struct ServicesSheetViewForParent: View {
             }
             .fullScreenCover(isPresented: $showApprovedMArkets) {
                 ApprovedMarketsView {
-            
+                    dismiss()
+                }
+            }
             .fullScreenCover(isPresented: $showChoosenLocations) {
                 
                 LocationPickerScreen(closure: {
                     dismiss()
                 })
             }
-
-        
             
         }
     }
-    
-    func handleServiceTap(_ service: ServicesType) {
-        switch service {
-        case .transfertoFamilyCard:
-            showTransactionSheet.toggle()
-        case .setDailySpending:
-            showLimitationSheet.toggle()
-        case .chooseLocatiion:
-            showChoosenLocations.toggle()
-        case .block:
-            showBlockCardSheet.toggle()
-        case .selectBackgroundImage:
-            showBackgroundPicker.toggle()
-        case .selectApprovedMArkets:
-            showApprovedMArkets.toggle()
-        default:
-            break
+        
+        func handleServiceTap(_ service: ServicesType) {
+            switch service {
+            case .transfertoFamilyCard:
+                showTransactionSheet.toggle()
+            case .setDailySpending:
+                showLimitationSheet.toggle()
+            case .chooseLocatiion:
+                showChoosenLocations.toggle()
+            case .block:
+                showBlockCardSheet.toggle()
+            case .selectBackgroundImage:
+                showBackgroundPicker.toggle()
+            case .selectApprovedMArkets:
+                showApprovedMArkets.toggle()
+            default:
+                break
+            }
+            
+            
         }
-        
-        
-    }
+    
 }
-
 
 #Preview {
     ServicesSheetViewForParent(id: "1")
