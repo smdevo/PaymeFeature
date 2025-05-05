72753c050494bdb75d17bde303fb89963ce24d14
Samandar <samandartoshpulatov0704@gmail.com>
2025-05-05 10:17:27 +0500

    adding fixation

--- PaymeFeature/PresentationLayer/UniteScreen(Tabbar)/MonitringScreen/Views/TransactionModel.swift
+++
@@ -1,9 +0,0 @@
-struct TransactionModel: Identifiable {
-    let id = UUID()
-    let date: String
-    let time: String
-    let amount: String
-    let description: String
-    let iconName: String?
-    let category: String?
-}
